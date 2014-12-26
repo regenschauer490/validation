2d99ca9e588393b882a879fb8bffa3be705631eb
Akihiro Nishimura <hagure.liminality@hotmail.co.jp>
2014-12-26 10:11:19 +0900

    add: #define BOOST_UBLAS_NDEBUG

--- vector_matrix/setting.hpp
+++ vector_matrix/setting.hpp
@@ -1,14 +1,15 @@
 ﻿#pragma once
 
+#define NDEBUG
+//#define _DEBUG
+#define _SCL_SECURE_NO_WARNINGS
+#define BOOST_UBLAS_NDEBUG
+#define EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET
+
 #include "SigUtil/lib/tools/random.hpp"
 #include "SigUtil/lib/tools/time_watch.hpp"
 #include "SigUtil/lib/calculation/basic_statistics.hpp"
 #include <fstream>
-
-#define NDEBUG
-#define _DEBUG
-#define _SCL_SECURE_NO_WARNINGS
-#define EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET
 
 using uint = std::size_t;
 
